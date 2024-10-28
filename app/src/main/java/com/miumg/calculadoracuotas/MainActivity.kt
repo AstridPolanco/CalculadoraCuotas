6d142120bde41b3683b1117637dae7776a51931b
mlyyppol <apenap1@miumg.edu.gt>
2024-10-28 13:21:20 -0600

    Cambios

--- app/src/main/java/com/miumg/calculadoracuotas/MainActivity.kt
+++ app/src/main/java/com/miumg/calculadoracuotas/MainActivity.kt
@@ -21,60 +21,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.miumg.calculadoracuotas.ui.theme.CalculadoraCuotasTheme
+import com.miumg.calculadoracuotas.views.HomeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-            LoanCalculatorScreen()
+            HomeView()
             }
         }
 
-}
-
-@Composable
-fun LoanCalculatorScreen() {
-    var principal by remember { mutableStateOf("") }
-    var interestRate by remember { mutableStateOf("") }
-    var term by remember { mutableStateOf("") }
-    var monthlyPayment by remember { mutableStateOf("") }
-
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        TextField(
-            value = principal,
-            onValueChange = { principal = it },
-            label = { Text("Principal") }
-        )
-        // ... otros campos de texto ...
-        Button(onClick = {
-            // Lógica de cálculo aquí
-            val calculatedPayment = calculateMonthlyPayment(principal.toDouble(), interestRate.toDouble(), term.toInt())
-            monthlyPayment = calculatedPayment.toString()
-        }) {
-            Text("Calcular")
-        }
-        Text("Cuota mensual: $monthlyPayment")
-    }
-}
-
-fun calculateMonthlyPayment(principal: Double, annualInterestRate: Double, termInMonths: Int): Double {
-    // Fórmula para calcular la cuota mensual
-    val monthlyInterestRate = annualInterestRate / 12 / 100
-    val numerator = principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, termInMonths.toDouble())
-    val denominator = Math.pow(1 + monthlyInterestRate, termInMonths.toDouble()) - 1
-    return numerator / denominator
-}
-
-
-@Composable
-fun GreetingPreview() {
-    CalculadoraCuotasTheme {
-
-    }
 }
